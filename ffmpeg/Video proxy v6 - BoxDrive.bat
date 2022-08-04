5d84ed129655447ea9179735a711cc160e4eaa93
5D'iraptor <5diraptor@gmail.com>
2022-08-04 13:49:39 +0100

    Update Video proxy v6 - BoxDrive.bat

--- ffmpeg/Video proxy v6 - BoxDrive.bat
+++ ffmpeg/Video proxy v6 - BoxDrive.bat
@@ -2,6 +2,6 @@
   CD "%~p1"
   MD "Proxy"
   attrib +h Proxy /s /d
-    for %%a in ("*.MOV") do if not exist "Proxy/%%~na.mp4" ffmpeg -i "%%a" -i "C:\Users\tom.morton\Box\Batch Processing\ffmpeg\proxy.png" -filter_complex "overlay=0:0" -q:v 20 -q:a 0  "Proxy/%%~na.mp4"
+    for %%a in ("*.MOV") do if not exist "Proxy/%%~na.mp4" ffmpeg -i "%%a" -i "C:\Users\%username%\Box\Batch Processing\ffmpeg\proxy.png" -filter_complex "overlay=0:0" -q:v 20 -q:a 0  "Proxy/%%~na.mp4"
   attrib -h Proxy /s /d
 PAUSE
